883e5f0dac18f086b25de6848d543f58847180f0
sujeewasandeepa <sujeewasandeepa@protonmail.ch>
2022-09-11 13:25:42 +0530

    create the button for the forum

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,19 @@
+import { Button } from "./Button"
 import { DropDown } from "./DropDown"
 
-export const Form = ()=> {
+export const Form = () => {
     let programmes = ["Software Engineering", "Computer Science", "Textile Engineering"]
     let batch_year = ["2020", "2021", "2022"]
     let batch_number = ["01", "02", "03"]
 
     return (
-        <div> 
-             <DropDown values ={programmes}> </DropDown>
+        <form> 
+             <DropDown values ={programmes}></DropDown>
              <DropDown values={batch_year}></DropDown>
              <DropDown values={batch_number}></DropDown>
-        </div>
+
+             <Button />
+        </form>
     )
 
 }
