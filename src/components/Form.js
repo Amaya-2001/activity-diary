4728e2385a07fbe4afe8250c4ec2d6a0b58b42a3
sujeewasandeepa <sujeewasandeepa@protonmail.ch>
2022-09-12 10:12:26 +0530

    create three useStates to catch drop down values

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,14 +4,18 @@ import FormStyles from "../form.module.css"
 
 export const Form = () => {
     let programmes = ["Software Engineering", "Computer Science", "Textile Engineering"]
-    let batch_year = ["2020", "2021", "2022"]
-    let batch_number = ["01", "02", "03"]
+    let batchYear = ["2020", "2021", "2022"]
+    let batchNumber = ["01", "02", "03"]
+
+    const [programme, setProgramme] = useState('')
+    const [year, setYear] = useState('')
+    const [batchNo, setBatchNo] = useState('')
 
     return (
         <form className={FormStyles.form}> 
              <DropDown values={programmes}></DropDown>
-             <DropDown values={batch_year}></DropDown>
-             <DropDown values={batch_number}></DropDown>
+             <DropDown values={batchYear}></DropDown>
+             <DropDown values={batchNumber}></DropDown>
 
              <Button />
         </form>
